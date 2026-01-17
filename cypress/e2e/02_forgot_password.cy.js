6e389df6e2040cbfa31073a210698a517b6ce1ad
dinavio <dinaviolina99@gmailcom>
2026-01-17 20:59:29 +0700

    new

--- cypress/e2e/02_forgot_password.cy.js
+++ cypress/e2e/02_forgot_password.cy.js
@@ -7,9 +7,9 @@ describe('Forgot Password - Orange HRM', ()=>{
     beforeEach(() => {
     // buka login page
     LoginPage.visit()
-    forgotPasswordPage.clickForgotPassword()
+    // forgotPasswordPage.interceptReset();
+    forgotPasswordPage.clickForgotPassword();
     // intercept API reset password
-    cy.intercept('POST', '**/requestPasswordResetCode').as('resetPassword')
     // forgotPasswordPage.verifyForgotPasswordPage()
 })
             
@@ -18,41 +18,27 @@ describe('Forgot Password - Orange HRM', ()=>{
         // forgotPasswordPage.verifyPageHeader()
     })
 
-    it('2. TC_FPW_002 Reset password using valid username',()=>{
+it('2. TC_FPW_002 Reset password using valid username',()=>{
         
         forgotPasswordPage.inputUsername(testData.validUsername)
         // cy.intercept('**/sendPasswordReset').as('resetPwUsername')
         forgotPasswordPage.clickReset()
-        cy.wait('@resetPassword', { timeout: 10000 }).then((interception) => {
-        console.log('Intercept response:', interception.response)
-        expect(interception.response.statusCode).to.eq(200)
-        })
-        forgotPasswordPage.verifySuccessMessage()
+        // forgotPasswordPage.waitReset()
+        forgotPasswordPage.verifySuccess()
+    })
+    it('3. TC_FPW_003 Reset password using invalid username', () => {
+    forgotPasswordPage.inputUsername(testData.invalidUsername)
+    forgotPasswordPage.clickReset()
+    forgotPasswordPage.verifySuccessMessage()
+    })
+    it('4. TC_FPW_004 Reset password using special character', () => {
+    forgotPasswordPage.inputUsername('@#@')
+    forgotPasswordPage.clickReset()
+    forgotPasswordPage.verifySuccessMessage()
     })
 
-    it('3. TC_FPW_003 Reset password using invalid username',()=>{
-        
-        forgotPasswordPage.inputUsername(testData.invalidUsername)
-        forgotPasswordPage.clickReset()
-        cy.wait('@resetPassword', { timeout: 10000 }).then((interception) => {
-        console.log('Intercept response:', interception.response)
-        expect(interception.response.statusCode).to.eq(200)
-        })
-        forgotPasswordPage.verifySuccessMessage()
-    })
 
-    it('4. TC_FPW_004 Reset password using character special',()=>{
-        
-        forgotPasswordPage.inputUsername('@#@')
-        
-        forgotPasswordPage.clickReset()
-        cy.wait('@resetPassword', { timeout: 10000 }).then((interception) => {
-        console.log('Intercept response:', interception.response)
-        expect(interception.response.statusCode).to.eq(200)
-        })
-        forgotPasswordPage.verifySuccessMessage()
-
-    })
+    
     it('5. TC_FPW_005 Click Button Cancel',()=>{
         forgotPasswordPage.clickCancel()
         cy.url().should('include', '/auth/login')
@@ -61,34 +47,21 @@ describe('Forgot Password - Orange HRM', ()=>{
         cy.reload()
         cy.get('input[name="username"]').should('have.value', '')
     })
-
     it('7. TC_FPW_007 Submit empty username (required validation)', () => {
-        forgotPasswordPage.clickReset()
-        forgotPasswordPage.verifyRequiredValidation()
+    forgotPasswordPage.clickReset()
+    forgotPasswordPage.verifyRequiredValidation()
     })
-
     it('8. TC_FPW_008 Reset password using numeric username', () => {
-        forgotPasswordPage.inputUsername('123456')
-        
-        forgotPasswordPage.clickReset()
-        cy.wait('@resetPassword', { timeout: 10000 }).then((interception) => {
-        console.log('Intercept response:', interception.response)
-        expect(interception.response.statusCode).to.eq(200)
-        })
-        forgotPasswordPage.verifySuccessMessage()
-
+    forgotPasswordPage.inputUsername('123456')
+    forgotPasswordPage.clickReset()
+    forgotPasswordPage.verifySuccessMessage()
     })
 
     it('9. TC_FPW_009 Reset password using username with space', () => {
-        forgotPasswordPage.inputUsername(' Admin ')
-        forgotPasswordPage.clickReset()
-        cy.wait('@resetPassword', { timeout: 10000 }).then((interception) => {
-        console.log('Intercept response:', interception.response)
-        expect(interception.response.statusCode).to.eq(200)
-        })
-        forgotPasswordPage.verifySuccessMessage()
+    forgotPasswordPage.inputUsername(' Admin ')
+    forgotPasswordPage.clickReset()
+    forgotPasswordPage.verifySuccessMessage()
     })
-
 
     it('10. TC_FPW_010 Navigate back after forgot password page', () => {
         cy.go('back')
--- cypress/support/pageObjects/forgotPasswordPage.js
+++ cypress/support/pageObjects/forgotPasswordPage.js
@@ -7,7 +7,7 @@ class ForgotPasswordPage {
     successMessage: '.oxd-text--h6',
     requiredMessage: '.oxd-input-group__message',
     forgotPasswordPageHeader: '.orangehrm-forgot-password-title',
-    forgotpageHeader: '.oxd-text oxd-text--p orangehrm-login-forgot-header',
+    forgotpageHeader: '.oxd-text.oxd-text--p.orangehrm-login-forgot-header',
     forgotLink: 'p.orangehrm-login-forgot-header'    
 }
 
@@ -25,7 +25,13 @@ class ForgotPasswordPage {
   clickCancel() {
     cy.get(this.selectors.cancelButton).click()
   }
+interceptReset(){
+    cy.intercept('POST', '**/requestPasswordResetCode').as('resetPassword')
 
+}
+  waitReset(){
+    cy.wait('@resetPassword', { timeout: 10000 })
+  }
 //   verifyPageHeader() {
 //     // cy.get(this.selectors.forgotpageHeader).should('be.visible')
 //     cy.get(this.selectors.forgotpageHeader).should('be.visible')
@@ -35,27 +41,31 @@ class ForgotPasswordPage {
 //    cy.get('div.orangehrm-login-forgot').click()
 
 //   }
-clickForgotPassword() {
-  cy.get('div.orangehrm-login-forgot')
-    // .should('be.visible')  // pastikan element terlihat
-    .click()
-  cy.url().should('include', '/requestPasswordResetCode')  // pastikan halaman sudah berubah
+    clickForgotPassword() {
+        cy.get('div.orangehrm-login-forgot').click()
+            // .should('be.visible')  // pastikan element terlihat
+            
+        // cy.url().should('include', '/requestPasswordResetCode')  // pastikan halaman sudah berubah
+    }
+
+    forgot(){
+        cy.get(this.selectors.forgotLink).click()
+    }
+
+    // verifySuccessMessage() {
+    //     cy.contains('Reset Password link sent successfully').should('be.visible')
+    // }
+    verifySuccess() {
+  cy.get(this.selectors.successMessage)
+    .should('be.visible')
 }
 
 
-  forgot(){
-    cy.get(this.selectors.forgotLink).click()
-  }
-
-  verifySuccessMessage() {
-    cy.contains('Reset Password link sent successfully').should('be.visible')
-  }
-
-  verifyRequiredValidation() {
-    cy.get(this.selectors.requiredMessage)
-      .should('be.visible')
-      .and('contain', 'Required')
-  }
-}
+    verifyRequiredValidation() {
+        cy.get(this.selectors.requiredMessage)
+        .should('be.visible')
+        .and('contain', 'Required')
+    }
+    }
 
 export default new ForgotPasswordPage()
